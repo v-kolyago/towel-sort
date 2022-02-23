605df11f5fefa3de35a8404617686210086384e8
vkolyago <v.kolyago@artox.com>
2022-02-23 22:38:19 +0300

    feat:finish to work with task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 
 // You should implement your task here.
 
+const { re } = require("semver");
+
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) {
+    return [];
+  }
+  else{
+  for(let i = 0; i<matrix.length; i++) {
+    if(i%2 !=0) {
+      matrix[i] = matrix[i].reverse();
+    }
+    else {
+      matrix[i];
+    }
+  }
+  var result = JSON.parse("[" + matrix + "]");
+      return result;
 }
+    }
